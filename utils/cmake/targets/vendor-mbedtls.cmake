2261353f5eed02f5dd18ac6743c92df8ee8e941e
Marek Kulik <me@botder.com>
2021-01-12 21:57:45 +0100

    Add mbedtls CMake

---
+++ utils/cmake/targets/vendor-mbedtls.cmake
@@ -0,0 +1,21 @@
+#
+# mbedtls library target
+#
+if (MTASA_OS_WINDOWS)
+    return()
+endif()
+
+set(VENDOR_MBEDTLS_DIR "${MTASA_VENDOR_DIR}/mbedtls")
+
+file(GLOB_RECURSE MBEDTLS_SOURCES LIST_DIRECTORIES false CONFIGURE_DEPENDS
+    "${VENDOR_MBEDTLS_DIR}/*.c"
+    "${VENDOR_MBEDTLS_DIR}/*.h"
+)
+
+add_library(vendor-mbedtls STATIC ${MBEDTLS_SOURCES})
+
+target_compile_definitions(vendor-mbedtls PUBLIC MBEDTLS_ZLIB_SUPPORT)
+
+target_include_directories(vendor-mbedtls PUBLIC "${VENDOR_MBEDTLS_DIR}/include")
+
+target_link_libraries(vendor-mbedtls PRIVATE vendor-zlib)

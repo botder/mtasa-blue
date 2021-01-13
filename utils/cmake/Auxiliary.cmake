6109c7544ca0d2e0b9ea60d0d46177ec964f1634
Marek Kulik <me@botder.com>
2021-01-13 03:54:39 +0100

    Add auxiliary CMake function

--- utils/cmake/Auxiliary.cmake
+++ utils/cmake/Auxiliary.cmake
@@ -21,7 +21,7 @@ endfunction()
 # Enables the permissive-flag (standards conformance), if compiled with a MSVC compiler
 #
 function(mtasa_target_enable_msvc_permissive TargetName)
-    if (TARGET ${TargetName} AND MTASA_MSVC)
+    if (MTASA_MSVC)
         target_compile_options(${TargetName} PRIVATE "/permissive-")
     endif()
 endfunction()
@@ -30,7 +30,7 @@ endfunction()
 # Applies per-monitor DPI Awareness in the Visual Studio target project properties
 #
 function(mtasa_target_set_dpi_aware TargetName)
-    if (TARGET ${TargetName} AND MTASA_VISUALSTUDIO)
+    if (MTASA_VISUALSTUDIO)
         set_property(TARGET ${TargetName} PROPERTY VS_DPI_AWARE "PerMonitor")
     endif()
 endfunction()
@@ -49,4 +49,13 @@ endmacro()
 macro(mtasa_add_static_library TargetName ObjectTargetName)
     add_library("${TargetName}" STATIC $<TARGET_OBJECTS:${ObjectTargetName}>)
     target_include_directories("${TargetName}" PUBLIC $<TARGET_PROPERTY:${ObjectTargetName},INTERFACE_INCLUDE_DIRECTORIES>)
+endmacro()
+
+#
+# Links the object library target to the given target
+#
+macro(mtasa_link_object_library TargetName ObjectTargetName)
+    target_sources("${TargetName}" PRIVATE $<TARGET_OBJECTS:${ObjectTargetName}>)
+    target_include_directories("${TargetName}" PRIVATE $<TARGET_PROPERTY:${ObjectTargetName},INTERFACE_INCLUDE_DIRECTORIES>)
+    target_compile_definitions("${TargetName}" PRIVATE $<TARGET_PROPERTY:${ObjectTargetName},INTERFACE_COMPILE_DEFINITIONS>)
 endmacro()

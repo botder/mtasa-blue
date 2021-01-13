6b2dd8587f136cd0f60d823ba194784c60986e36
Marek Kulik <me@botder.com>
2021-01-13 11:29:24 +0100

    Fix MySQL CMake

--- utils/cmake/targets/vendor-mysql.cmake
+++ utils/cmake/targets/vendor-mysql.cmake
@@ -8,19 +8,17 @@ add_library(vendor-mysql INTERFACE)
 if (MTASA_OS_WINDOWS)
     set_target_properties(vendor-mysql PROPERTIES
         IMPORTED_IMPLIB
-
-        $<IF:$<BOOL:${MTASA_X64}>
-            "${VENDOR_MYSQL_DIR}/lib/x64/libmysql.lib"
-            ,
-            "${VENDOR_MYSQL_DIR}/lib/x86/libmysql.lib"
+        $<IF:
+            $<BOOL:${MTASA_X64}>
+            , "${VENDOR_MYSQL_DIR}/lib/x64/libmysql.lib"
+            , "${VENDOR_MYSQL_DIR}/lib/x86/libmysql.lib"
         >
 
         IMPORTED_LOCATION
-        
-        $<IF:$<BOOL:${MTASA_X64}>
-            "${VENDOR_MYSQL_DIR}/lib/x64/libmysql.dll"
-            ,
-            "${VENDOR_MYSQL_DIR}/lib/x86/libmysql.dll"
+        $<IF:
+            $<BOOL:${MTASA_X64}>
+            , "${VENDOR_MYSQL_DIR}/lib/x64/libmysql.dll"
+            , "${VENDOR_MYSQL_DIR}/lib/x86/libmysql.dll"
         >
     )
 endif()

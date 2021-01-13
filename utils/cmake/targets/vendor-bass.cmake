fcd3b2cdac25f7a6a5915f3221eedee93e58a719
Marek Kulik <me@botder.com>
2021-01-13 11:30:00 +0100

    Fix BASS CMake

--- utils/cmake/targets/vendor-bass.cmake
+++ utils/cmake/targets/vendor-bass.cmake
@@ -7,13 +7,13 @@ add_library(vendor-bass-sdk INTERFACE)
 target_include_directories(vendor-bass-sdk INTERFACE "${VENDOR_BASS_DIR}")
 
 add_library(vendor-bass STATIC IMPORTED)
-set_property(TARGET vendor-bass PROPERTY IMPORTED_IMPLIB "${VENDOR_BASS_DIR}/lib/bass.lib")
+set_property(TARGET vendor-bass PROPERTY IMPORTED_LOCATION "${VENDOR_BASS_DIR}/lib/bass.lib")
 
 add_library(vendor-bass-fx STATIC IMPORTED)
-set_property(TARGET vendor-bass-fx PROPERTY IMPORTED_IMPLIB "${VENDOR_BASS_DIR}/lib/bass_fx.lib")
+set_property(TARGET vendor-bass-fx PROPERTY IMPORTED_LOCATION "${VENDOR_BASS_DIR}/lib/bass_fx.lib")
 
 add_library(vendor-bass-mix STATIC IMPORTED)
-set_property(TARGET vendor-bass-mix PROPERTY IMPORTED_IMPLIB "${VENDOR_BASS_DIR}/lib/bassmix.lib")
+set_property(TARGET vendor-bass-mix PROPERTY IMPORTED_LOCATION "${VENDOR_BASS_DIR}/lib/bassmix.lib")
 
 add_library(vendor-bass-tags STATIC IMPORTED)
-set_property(TARGET vendor-bass-tags PROPERTY IMPORTED_IMPLIB "${VENDOR_BASS_DIR}/lib/tags.lib")
+set_property(TARGET vendor-bass-tags PROPERTY IMPORTED_LOCATION "${VENDOR_BASS_DIR}/lib/tags.lib")

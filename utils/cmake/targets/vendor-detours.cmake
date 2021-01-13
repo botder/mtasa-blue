fc0c59ff7dfafb0428aec3b49410cf7001a3ac56
Marek Kulik <me@botder.com>
2021-01-13 03:48:24 +0100

    Add detours CMake

---
+++ utils/cmake/targets/vendor-detours.cmake
@@ -0,0 +1,17 @@
+#
+# Detours library target
+#
+if (NOT MTASA_OS_WINDOWS)
+    return()
+endif()
+
+set(VENDOR_DETOURS_DIR "${MTASA_VENDOR_DIR}/detours")
+
+add_library(vendor-detours STATIC IMPORTED)
+
+set_target_properties(vendor-detours PROPERTIES
+    IMPORTED_IMPLIB
+    "${VENDOR_DETOURS_DIR}/lib/detours.lib"
+)
+
+target_include_directories(vendor-detours INTERFACE "${VENDOR_DETOURS_DIR}/include")

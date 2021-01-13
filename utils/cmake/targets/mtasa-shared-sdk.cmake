941a60a50895410057e576716c4b04c3e4c5e73f
Marek Kulik <me@botder.com>
2021-01-13 03:53:45 +0100

    Add Shared SDK CMake

---
+++ utils/cmake/targets/mtasa-shared-sdk.cmake
@@ -0,0 +1,8 @@
+#
+# Shared SDK library target
+#
+set(SHARED_SDK_DIR "${MTASA_SHARED_DIR}/sdk")
+
+add_library(mtasa-shared-sdk INTERFACE)
+
+target_include_directories(mtasa-shared-sdk INTERFACE "${SHARED_SDK_DIR}")

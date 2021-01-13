747e81f97966415e32decf3f2a7c62d74db31142
Marek Kulik <me@botder.com>
2021-01-13 11:30:34 +0100

    Add vendor include directory to shared SDK

--- utils/cmake/targets/mtasa-shared-sdk.cmake
+++ utils/cmake/targets/mtasa-shared-sdk.cmake
@@ -5,4 +5,7 @@ set(SHARED_SDK_DIR "${MTASA_SHARED_DIR}/sdk")
 
 add_library(mtasa-shared-sdk INTERFACE)
 
-target_include_directories(mtasa-shared-sdk INTERFACE "${SHARED_SDK_DIR}")
+target_include_directories(mtasa-shared-sdk INTERFACE
+    "${SHARED_SDK_DIR}"
+    "${MTASA_VENDOR_DIR}"
+)

57c59477530e2b7e5d61dd9639c0ec02b38e1ef9
Marek Kulik <me@botder.com>
2021-01-14 01:47:54 +0100

    Fix pthread CMake

--- utils/cmake/targets/vendor-pthread.cmake
+++ utils/cmake/targets/vendor-pthread.cmake
@@ -1,17 +1,31 @@
 #
 # pthread library target
 #
+if (NOT MTASA_OS_WINDOWS)
+    return()
+endif()
+
 set(VENDOR_PTHREAD_DIR "${MTASA_VENDOR_DIR}/pthreads")
 
-add_library(vendor-pthread STATIC "${VENDOR_PTHREAD_DIR}/include/pthread.c")
+add_library(vendor-pthread STATIC
+    "${VENDOR_PTHREAD_DIR}/include/pthread.c"
+    "${VENDOR_PTHREAD_DIR}/include/pthread.h"
+    "${VENDOR_PTHREAD_DIR}/include/config.h"
+    "${VENDOR_PTHREAD_DIR}/include/context.h"
+    "${VENDOR_PTHREAD_DIR}/include/implement.h"
+    "${VENDOR_PTHREAD_DIR}/include/need_errno.h"
+    "${VENDOR_PTHREAD_DIR}/include/sched.h"
+    "${VENDOR_PTHREAD_DIR}/include/semaphore.h"
+)
 
 target_include_directories(vendor-pthread PUBLIC "${VENDOR_PTHREAD_DIR}/include")
 
-target_compile_definitions(vendor-pthread PUBLIC
-    _TIMESPEC_DEFINED
+target_compile_definitions(vendor-pthread
+    PRIVATE
+        PTW32_STATIC_LIB
 
-    $<$<BOOL:${MTASA_OS_WINDOWS}>:
+    PUBLIC
+        _TIMESPEC_DEFINED
         HAVE_PTW32_CONFIG_H
         PTW32_BUILD_INLINED
-    >
 )

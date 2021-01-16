8775f0124df5c4a41741b5d13b7248714aabc4aa
Marek Kulik <me@botder.com>
2021-01-16 19:42:58 +0100

    Fix compile options in CMake

--- utils/cmake/compilers/MSVC.cmake
+++ utils/cmake/compilers/MSVC.cmake
@@ -10,14 +10,12 @@ else()
     set(MTASA_MSVC_NO_FRONTEND TRUE)
 endif()
 
-add_compile_options(
-    # See https://docs.microsoft.com/en-us/cpp/build/reference/zc-cplusplus
-    # See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
-    /Zc:__cplusplus
-)
-
 add_compile_definitions(
     _CRT_SECURE_NO_WARNINGS
     _SCL_SECURE_NO_WARNINGS
     _CRT_NONSTDC_NO_DEPRECATE
 )
+
+# See https://docs.microsoft.com/en-us/cpp/build/reference/zc-cplusplus
+# See https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+list(APPEND MTASA_COMPILE_OPTIONS "/Zc:__cplusplus")

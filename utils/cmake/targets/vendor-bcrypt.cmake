05d35dc036eaaa1f3328b7a0953b59bbec36c261
Marek Kulik <me@botder.com>
2021-01-16 19:44:51 +0100

    Remove GLOB in bcrypt CMake

--- utils/cmake/targets/vendor-bcrypt.cmake
+++ utils/cmake/targets/vendor-bcrypt.cmake
@@ -3,11 +3,15 @@
 #
 set(VENDOR_BCRYPT_DIR "${MTASA_VENDOR_DIR}/bcrypt")
 
-file(GLOB_RECURSE BCRYPT_SOURCES LIST_DIRECTORIES false CONFIGURE_DEPENDS
-    "${VENDOR_BCRYPT_DIR}/*.c"
-    "${VENDOR_BCRYPT_DIR}/*.h"
+add_library(vendor-bcrypt STATIC
+    "${VENDOR_BCRYPT_DIR}/crypt_blowfish.c"
+    "${VENDOR_BCRYPT_DIR}/crypt_gensalt.c"
+    "${VENDOR_BCRYPT_DIR}/wrapper.c"
+    "${VENDOR_BCRYPT_DIR}/crypt_blowfish.h"
+    "${VENDOR_BCRYPT_DIR}/crypt_gensalt.h"
+    "${VENDOR_BCRYPT_DIR}/crypt.h"
+    "${VENDOR_BCRYPT_DIR}/ow-crypt.h"
+    "${VENDOR_BCRYPT_DIR}/x86.S"
 )
-
-add_library(vendor-bcrypt STATIC ${BCRYPT_SOURCES})
 
 target_include_directories(vendor-bcrypt PUBLIC "${VENDOR_BCRYPT_DIR}")

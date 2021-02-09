8c1acbc746b1dd6dc38eb634aad48a74a4062995
Marek Kulik <me@botder.com>
2021-02-09 23:17:25 +0100

    Use unity build for server launcher

---
+++ Server/launcher/UnityBuild.cpp
@@ -0,0 +1,12 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  PURPOSE:     Unity build file
+ *
+ *  Multi Theft Auto is available from https://multitheftauto.com/
+ *
+ *****************************************************************************/
+
+#include "Main.cpp"
+#include "CDynamicLibrary.cpp"
--- Server/launcher/premake5.lua
+++ Server/launcher/premake5.lua
@@ -1,44 +1,34 @@
 project "Launcher"
-	language "C++"
-	kind "ConsoleApp"
-	targetdir(buildpath("server"))
+    language "C++"
+    kind "ConsoleApp"
+    targetdir(buildpath("server"))
 
-	includedirs { "../sdk" }
+    includedirs {
+        "../sdk",
+    }
+    
+    files {
+        "premake5.lua",
+        "UnityBuild.cpp",
+    }
 
-	vpaths {
-		["Headers/*"] = "**.h",
-		["Sources/*"] = "**.cpp",
-		["Resources/*"] = {"*.rc", "**.ico"},
-		["*"] = "premake5.lua"
-	}
+    filter "system:windows"
+        targetname "MTA Server"
+        staticruntime "On"
 
-	files {
-		"premake5.lua",
-		"*.h",
-		"*.cpp"
-	}
+        files {
+            "launcher.rc",
+        }
 
-	filter "system:windows"
-		targetname "MTA Server"
-		staticruntime "On"
-		files {
-			"launcher.rc",
-			"resource/mtaicon.ico"
-		}
+    filter {"system:windows", "platforms:x64"}
+        targetname "MTA Server64"
 
-	filter "system:not windows"
-		links { "dl" }
-		buildoptions { "-pthread", "-fvisibility=default" }
-		linkoptions { "-pthread", "-rdynamic" }
+    filter "system:not windows"
+        targetname "mta-server"
 
-	filter {"system:linux", "platforms:x86"}
-		targetname "mta-server"
+        links { "dl" }
+        buildoptions { "-pthread", "-fvisibility=default" }
+        linkoptions { "-pthread", "-rdynamic" }
 
-	filter {"system:linux", "platforms:x64"}
-		targetname "mta-server64"
-
-	filter {"system:macosx"}
-		targetname "mta-server"
-
-	filter {"system:windows", "platforms:x64"}
-		targetname "MTA Server64"
+    filter {"system:linux", "platforms:x64"}
+        targetname "mta-server64"
